73472b2935f8c67a31ee338923d1cdec7397d8f4
Akif397 <akifmuhtasim397@gmail.com>
2020-11-30 23:57:17 +0600

    commit from home

--- movie-rater/src/components/movie-details.js
+++ movie-rater/src/components/movie-details.js
@@ -8,9 +8,22 @@ function MovieDetails(props) {
 
     const movie = props.movies;
 
-    const [ highlighted, setHighlighted ] = useState(-1);
+    const [highlighted, setHighlighted] = useState(-1);
     const highlightRate = high => evt => {
         setHighlighted(high);
+    }
+    const rateClicked = rate => evt => {
+        fetch(`http://127.0.0.1:8000/api/movies/${movie.id}/rate_movie/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;odata=verbose',
+                'Authorization': 'Token 587f51b1fd5fac03914b5d9794d857a1ccc1ff1d'
+            },
+            body: JSON.stringify( {stars: rate + 1} )
+        })
+            .then(resp => resp.json())
+            .then(resp => console.log(resp))
+            .catch(error => console.log(error))
     }
 
     return (
@@ -19,18 +32,20 @@ function MovieDetails(props) {
                 <div>
                     <h1>{movie && movie.title}</h1>
                     <p>{movie && movie.desc}</p>
-                    <FontAwesomeIcon icon={ faStar } className={ movie.avg_rating > 0 ? "orange" : ""}/>
-                    <FontAwesomeIcon icon={ faStar } className={ movie.avg_rating > 1 ? "orange" : ""}/>
-                    <FontAwesomeIcon icon={ faStar } className={ movie.avg_rating > 2 ? "orange" : ""}/>
-                    <FontAwesomeIcon icon={ faStar } className={ movie.avg_rating > 3 ? "orange" : ""}/>
-                    <FontAwesomeIcon icon={ faStar } className={ movie.avg_rating > 4 ? "orange" : ""}/>
-                    ({ movie.no_of_ratings })
+                    <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 0 ? "orange" : ""} />
+                    <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 1 ? "orange" : ""} />
+                    <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 2 ? "orange" : ""} />
+                    <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 3 ? "orange" : ""} />
+                    <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 4 ? "orange" : ""} />
+                    ({ movie.no_of_ratings})
 
                     <div className="rate-container">
                         <h3>Rate It</h3>
                         {
-                            [...Array(5)].map( (element, index) => {
-                                return <FontAwesomeIcon key={ index } icon={ faStar } className={ highlighted > index - 1 ? "purple" : ""} onMouseEnter={ highlightRate(index) } onMouseLeave={ highlightRate(-1) }/>
+                            [...Array(5)].map((element, index) => {
+                                return <FontAwesomeIcon key={index} icon={faStar} className={highlighted > index - 1 ? "purple" : ""}
+                                    onMouseEnter={highlightRate(index)}
+                                    onMouseLeave={highlightRate(-1)} onClick={rateClicked(index)} />
                             })
                         }
                     </div>
